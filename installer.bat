5b64fcd2071062945385611253dcb918d21987b7
RG2N <64289295+RG2N@users.noreply.github.com>
2021-07-12 11:43:20 -0400

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,5 +1,6 @@
 RMDIR /s /q "%appdata%\.minecraft\versions\PolarClient"
 move "PolarClient" "%appdata%\.minecraft\versions"
 Taskkill /im MinecraftLauncher.exe
+del "PolarClient.zip"
 del "installer.bat"
 pause
